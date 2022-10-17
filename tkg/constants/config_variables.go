592f8892df622c300c281f4e0e6630bdd41b9c81
Christian Schlotter <christi.schlotter@gmail.com>
2022-10-17 17:10:12 +0200

    Add variables for Pod Security Standard overlay

--- tkg/constants/config_variables.go
+++ tkg/constants/config_variables.go
@@ -191,6 +191,11 @@ const (
 	EnableAuditLogging  = "ENABLE_AUDIT_LOGGING"
 	TKGIPFamily         = "TKG_IP_FAMILY"
 
+	PodSecurityStandardDisabled = "POD_SECURITY_STANDARD_DISABLED"
+	PodSecurityStandardAudit    = "POD_SECURITY_STANDARD_AUDIT"
+	PodSecurityStandardWarn     = "POD_SECURITY_STANDARD_WARN"
+	PodSecurityStandardEnforce  = "POD_SECURITY_STANDARD_ENFORCE"
+
 	ConfigVariableOSName    = "OS_NAME"
 	ConfigVariableOSVersion = "OS_VERSION"
 	ConfigVariableOSArch    = "OS_ARCH"
